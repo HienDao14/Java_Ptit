5f6d824909e68c00a2635dd55c8e1fa628b0be58
HienDao14 <145284233+HienDao14@users.noreply.github.com>
2023-10-08 20:46:41 +0700

    J01004.java

--- J01004.java
+++ J01004.java
@@ -1,15 +1,9 @@
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author Admin
+ * @author HienDao
  */
 public class J01004 {
     public static Scanner sc = new Scanner(System.in);
